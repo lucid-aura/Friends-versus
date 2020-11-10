ff88e4d3ab8ef289aaeaa68e1117c306f3c90d77
kangseokwoo <pko89403@gmail.com>
2020-11-10 18:37:20 +0900

    config 파일을 위한 수정 경로

--- config/config_test.py
+++ config/config_test.py
@@ -1,8 +1,8 @@
-from config import JsonConfig
+import sys 
+
+sys.path.append('..')
+from config.config import JsonConfig
 
 if __name__ == "__main__":
-    config = JsonConfig(configFile="Develop.json").config
-
+    config = JsonConfig(configFile='Develop.json').config
     print(config)
-    print(config["DATABASE"])
-    print(config["DATABASE"]["MONGODB"])
