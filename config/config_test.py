f235d0aa65d6fc6d09e5144006822b5c40d0a086
kangseokwoo <pko89403@gmail.com>
2020-11-23 23:14:19 +0900

    code reviewing outputs

--- config/config_test.py
+++ config/config_test.py
@@ -1,8 +1,9 @@
 import sys 
-
+from pprint import pprint 
 sys.path.append('..')
 from config import JsonConfig
 
 if __name__ == "__main__":
     config = JsonConfig(configFile='Develop.json').config
-    print(config)
+    pprint(config)
+
--- mongodb/Handler_test.py
+++ mongodb/Handler_test.py
@@ -8,6 +8,7 @@ sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__)))+"/hy
 from method import JsonParser
 
 
+
 host='192.168.40.52'
 port=27017
 username='root'
