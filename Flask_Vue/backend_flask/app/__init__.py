1b0c7ca635e351ec3ce4a40105ae24d6aad62ee5
kangseokwoo <pko89403@gmail.com>
2020-12-13 13:54:55 +0900

    create app as func

--- Flask_Vue/backend_flask/app/__init__.py
+++ Flask_Vue/backend_flask/app/__init__.py
@@ -3,9 +3,22 @@ import os
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__)))+"/../../config")
 from config import FlaskConfig
 from flask import Flask 
+from .views import main_views
+from flask_cors import CORS, cross_origin
 
-app = Flask(__name__)
-app.config['JSON_AS_ASCII'] = False # utf-8 제대로 표현
-app.config.from_object(FlaskConfig)
+def create_app():
+    app = Flask(import_name=__name__,
+                template_folder='templates')
+    
+    app.config['JSON_AS_ASCII'] = False # utf-8 제대로 표현    
+    app.config.from_object(FlaskConfig)
 
-from . import routes 
+    CORS(app, resources={r'*': {'origins': '*'}})
+
+    app.register_blueprint(main_views.bp)
+
+    return app 
+
+# from . import routes 
+
+
